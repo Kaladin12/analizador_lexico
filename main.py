90551e7bac8f50b5c32af8b9fe1d22b6295b84b6
Kaladin12 <eliancruz998@gmail.com>
2022-10-11 13:20:17 -0700

    think its done

--- main.py
+++ main.py
@@ -5,19 +5,10 @@ from fallo import fallo
 recupera = False
 encontro_lexema = False
 
-cadena = 'en caso  en imprimir longitud longitudes &50.2569 < predeterminado interruptor siw si sin sino sinowe -5  "tt4b"interruptor  &| "" cada cadaw cadena cadena542 (1245 e == 4565.2 _ } mientras-haz mientras regresar2 tomacar dormire < 5 importar rango romper romper2 < rango2'#  != verdadero    nulo    <= ==    cas    flotante e  < cadena5  falso siono nulo hugyefjne2?'
-# '  cadena cadena542   != verdadero    nulo    <= ==    cas    flotante e  < cadena5  falso siono nulo hugyefjne2' 
-super_cadena_original = cadena
-estado = 209
-inicio = 209
-final = 0
-inicio_subcadena = 0
-
 def buscar_lexema_en_diagrama(estado, caracter):
     global inicio_subcadena, encontro_lexema , final
     #print("estado:", estado, final)
     estado, regresa, tok = diagramas[inicio][0](estado, caracter) # Revisar si en base al caracter se mueve a otro estado o fallo (-1)    
-    s = super_cadena_original[inicio_subcadena:final]
     if not regresa: final += 1
     else: final = final -1 if caracter != " " else final # no tiene sentido quitar siempre un caracter
     #print(final)
@@ -26,6 +17,8 @@ def buscar_lexema_en_diagrama(estado, caracter):
             or 
             estado == diagramas[inicio][1]): # Si el estado esta en el estado de aceptacion correspondiente al diagrama
         #s = super_cadena_original[inicio_subcadena:final]
+        s = super_cadena_original[inicio_subcadena:final]
+    
         if inicio == 209:
             temp_tok = check_if_id_is_reserved_(s)
             if temp_tok!=None:  tok = temp_tok
@@ -82,25 +75,36 @@ def cosa(cadena, estado):
             return caracter + cadena
     return cadena
 
-cadena = cadena + ' '
-while len(cadena)!=0:
-    cadena = cosa(cadena, estado)
-    #print(encontro_lexema)
-    if inicio == None:
-        break
-    if not encontro_lexema:
-        inicio = fallo(inicio)
+cadenas = []
+with open(r"C:\Users\Yo\prueba.txt", encoding='utf-8') as f:
+    cadenas = f.read().splitlines()
+for cadena in cadenas:
+    #cadena = 'en caso  en imprimir longitud longitudes &50.2569 < predeterminado interruptor siw si sin sino sinowe -5  "tt4b"interruptor  &| "" cada cadaw cadena cadena542 (1245 e == 4565.2 _ } mientras-haz mientras regresar2 tomacar dormire < 5 importar rango romper romper2 < rango2'#  != verdadero    nulo    <= ==    cas    flotante e  < cadena5  falso siono nulo hugyefjne2?'
+    # '  cadena cadena542   != verdadero    nulo    <= ==    cas    flotante e  < cadena5  falso siono nulo hugyefjne2' 
+    super_cadena_original = cadena
+    estado = 209
+    inicio = 209
+    final = 0
+    inicio_subcadena = 0
+    cadena = cadena + ' '
+    while len(cadena)!=0:
+        cadena = cosa(cadena, estado)
+        #print(encontro_lexema)
         if inicio == None:
             break
-        estado = inicio
-        final = inicio_subcadena
-    #print('cadena: [', cadena, estado, encontro_lexema, recupera)
-    else:
-        inicio = 209
-        estado =  inicio
-        final = inicio_subcadena
-        recupera = False
-    #print('fuera',inicio_subcadena, final)
-    encontro_lexema = False
-    
-    
+        if not encontro_lexema:
+            inicio = fallo(inicio)
+            if inicio == None:
+                break
+            estado = inicio
+            final = inicio_subcadena
+        #print('cadena: [', cadena, estado, encontro_lexema, recupera)
+        else:
+            inicio = 209
+            estado =  inicio
+            final = inicio_subcadena
+            recupera = False
+        #print('fuera',inicio_subcadena, final)
+        encontro_lexema = False
+        
+        
---
+++ reader.py
@@ -0,0 +1,3 @@
+d = None
+with open(r"C:\Users\Yo\prueba.txt", encoding='utf-8') as f:
+    print(f.read().splitlines())
